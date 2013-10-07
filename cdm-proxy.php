22571ad8a3e767bd9532e23ec9c05ecfe7242f50
Larry Farrell <farrelldlfarrell@gmail.com>
2013-10-06 21:54:34 -0400

    CDM Collage

---
+++ cdm-proxy.php
@@ -0,0 +1,25 @@
+<?php
+$data = strip_tags(trim($_GET['q']));
+$terms = preg_split('\s+/', $data);
+$type = strip_tags(trim($_GET['type']));
+
+//$ch = curl_init("https://cdm_path/dmwebservices/index.php?q=dmQuery/all/subjec^" . $data . "^all^and/title/title/1024/1/0/0/0/0/0/0/" . $type);
+//curl_setopt($ch, CURLOPT_HEADER, 0);
+//$t = curl_exec($ch);
+//curl_close($ch);
+$t = file_get_contents("https://cdm_path:82/dmwebservices/index.php?q=dmQuery/all/subjec^" . $data . "^all^and/title/title/1024/1/0/0/0/0/0/0/" . $type);
+
+$e = json_decode($t);
+$thumbnails = array();
+
+$i = 0;
+foreach($e->records as $c) {
+    if(preg_match('/(jp2|png|jpg|jpeg)$/', $c->find)) {
+        $thumbnails[$i]['url'] = 'http://cdm_path/utils/getthumbnail/collection' . $c->collection . '/id/' . $c->pointer;
+        $thumbnails[$i]['title'] = $c->title;
+        $thumbnails[$i]['collection'] = $c->collection;
+        $thumbnails[$i]['pointer'] = $c->pointer;
+        $i++;
+    }
+}
+echo json_encode($thumbnails);
