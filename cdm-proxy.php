a31014c7be373a006a34813415955768bdd3e205
lfarrell <lfarrell@email.unc.edu>
2013-10-08 10:42:45 -0400

    switched to using curl

--- cdm-proxy.php
+++ cdm-proxy.php
@@ -3,11 +3,11 @@ $data = strip_tags(trim($_GET['q']));
 $terms = preg_split('\s+/', $data);
 $type = strip_tags(trim($_GET['type']));
 $cdm_path = 'cdm_path';
-//$ch = curl_init("https://cdm_path/dmwebservices/index.php?q=dmQuery/all/subjec^" . $data . "^all^and/title/title/1024/1/0/0/0/0/0/0/" . $type);
-//curl_setopt($ch, CURLOPT_HEADER, 0);
-//$t = curl_exec($ch);
-//curl_close($ch);
-$t = file_get_contents("https://$cdm_path:82/dmwebservices/index.php?q=dmQuery/all/subjec^" . $data . "^all^and/title/title/1024/1/0/0/0/0/0/0/" . $type);
+$ch = curl_init("https://$cdm_path:82/dmwebservices/index.php?q=dmQuery/all/subjec^" . $data . "^all^and/title/title/1024/1/0/0/0/0/0/0/json");
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$t = curl_exec($ch);
+curl_close($ch);
 
 $e = json_decode($t);
 $thumbnails = array();
